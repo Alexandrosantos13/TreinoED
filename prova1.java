76e3a006085478c3bf8e1122f7e48b3345a3c1f8
Alexandrosantos13 <alexandro.santos1305@gmail.com>
2025-11-16 21:24:30 -0300

    Letra B da questao 1 respondida

--- prova1.java
+++ prova1.java
@@ -4,7 +4,10 @@ public class prova1 {
     public static void main (String[] args) {
         List<Object> list1 = new ArrayList<>();
         List<Object> list2 = new ArrayList<>();
+        Collections.addAll(list1, 1, 2, 3, 4, 5);
+        Collections.addAll(list2, 3, 4, 5, 6, 7);
         Scanner sc = new Scanner(System.in);
+        
         
         while (true) {
             
@@ -35,6 +38,13 @@ public class prova1 {
                     sc.next(); 
                     sc.nextLine(); 
                     break;
+                    case 4:
+                    Object[] ex2 = letraB(list1, list2);
+                    System.out.println("Resultado: " + Arrays.toString(ex2));
+                    System.out.println("Pression3e Enter para continuar...");
+                    sc.next(); 
+                    sc.nextLine(); 
+                    break;
                 case 0:
                     System.out.println("Saindo...");
                     sc.close();
@@ -60,8 +70,41 @@ public class prova1 {
             }
             if (!isequal&&!alreadyhave){
                 temp.add(list1.get(i));
-                }
+            }
         }
+        return temp.toArray(new Object [0]);
+    }
+        public static <T> Object[] letraB(List <T> list1, List <T> list2){
+        List <T> temp = new LinkedList<>();
+        for (int i =0; i<list1.size(); i++){
+            boolean isequal = false;
+            boolean alreadyhave=false;
+            for (int j = 0; j<list2.size();j++){
+                    isequal=Objects.equals(list1.get(i),list2.get(j));
+                    alreadyhave=temp.contains(list1.get(i));
+                    if (isequal ||alreadyhave){
+                        break;
+                }
+            }
+            if (!isequal&&!alreadyhave){
+                temp.add(list1.get(i));
+            }
+        }
+        for (int j =0; j<list1.size(); j++){
+            boolean isequal = false;
+            boolean alreadyhave=false;
+            for (int i = 0; i<list2.size();i++){
+                    isequal=Objects.equals(list2.get(j),list1.get(i));
+                    alreadyhave=temp.contains(list2.get(j));
+                    if (isequal ||alreadyhave){
+                        break;
+                }
+            }
+            if (!isequal&&!alreadyhave){
+                temp.add(list2.get(j));
+            }
+        }
+        
         return temp.toArray(new Object [0]);
 }
 public static void clearScreen() {
